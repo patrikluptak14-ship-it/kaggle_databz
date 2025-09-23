25fb2bdbbd582d8d34b34dceeffe712e091916a3
Patrik Lupták <patrikluptak14@gmail.com>
2025-09-23 11:11:06 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+define("WEB_AUTOR", "Patrik");
+define("Company_name", "PRX");
+define("Company_title", "hicn cwin oisdn");
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include ("./config.php",1);?>
+<!DOCTYPE html>
+<html lang="sk">
+
+<head>
+
